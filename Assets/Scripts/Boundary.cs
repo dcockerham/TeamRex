42c2a9153152b65782d197c569662f3c2e29e9cf
AlvieHuang <Alvinh7@uci.edu>
2016-04-09 10:15:04 -0700

    Deleting extras

--- Assets/Scripts/Boundary.cs
+++ Assets/Scripts/Boundary.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class Boundary : MonoBehaviour {
 
-    void OnTriggerExit(Collider other)
+    void OnTriggerExit(Collider2D other)
     {
-        
+        Destroy(other.gameObject);
+        print(other.tag.ToString());
     }
 }
