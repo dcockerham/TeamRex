1ae5c08d1874e77bf7b59babb627d14d36ef0933
dcockerham <dylantchaos@gmail.com>
2016-04-08 22:17:13 -0700

    FIXFIXFIX

--- TobiiRex/Assets/EyeXDemos/SceneNavigationAlignment.cs
+++
@@ -1,9 +0,0 @@
-//-----------------------------------------------------------------------
-// Copyright 2014 Tobii Technology AB. All rights reserved.
-//-----------------------------------------------------------------------
-
-public enum SceneNavigationAlignment
-{
-	Top = 0,
-	Bottom = 1
-}
