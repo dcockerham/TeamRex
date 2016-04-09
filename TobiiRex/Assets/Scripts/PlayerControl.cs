ccaf3d9fbe4eedfae8c895a29a1a659f457bc577
dcockerham <dylantchaos@gmail.com>
2016-04-08 20:36:35 -0700

    The beginning begins!  Here's the project, for now, I guess.  Whatever.

---
+++ TobiiRex/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPos = gameObject.transform.position;
+
+		if (Input.GetKey (KeyCode.W)) {
+			newPos.y += 0.1f;
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			newPos.y -= 0.1f;
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			newPos.x -= 0.1f;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			newPos.x += 0.1f;
+		}
+
+		gameObject.transform.position = newPos;
+	}
+}
