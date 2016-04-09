27c2f90b5fee85a790e0abdf7d0abe937c519a71
clpachec <carlleninpacheco@gmail.com>
2016-04-09 12:09:37 -0700

    Game Over Added

--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -1,15 +1,53 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MainController : MonoBehaviour {
+    public int lives;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public Text scoreText;
+    public Text restartText;
+    public Text gameOverText;
+
+    // Use this for initialization
+    void Start () {
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        GameOver();
+   
+    }
+
+    private void GameOver()
+    {
+        if (lives <= 0)
+        {
+            gameOverText.text = "GameOver!";
+            restartText.text = "Press 'R' for Restart";
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                ReloadCurrentScene();
+            }
+        }
+    }
+
+    public void Death()
+    {
+        lives--;
+    }
+
+    private void ReloadCurrentScene()
+    {
+        // get the current scene name 
+        string sceneName = SceneManager.GetActiveScene().name;
+
+        // load the same scene
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+
 }
