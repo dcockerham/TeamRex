4316216500db30109c0c7004bb4ce119614560ad
Hiroshi Estrada <hlestrad@uci.edu>
2016-04-09 18:58:04 -0700

    Nightmare now spawns shit when destroyed

--- Assets/Scripts/Nightmare_Enemy.cs
+++ Assets/Scripts/Nightmare_Enemy.cs
@@ -1,21 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Nightmare_Enemy : MonoBehaviour {
 
 	Player_Controller player;
 	public bool freezing = false;
 
-	// Use this for initialization
-	void Start () {
-		player = GameObject.Find("Player").GetComponent<Player_Controller>();
+    public float speed = 2.5f;
+    public Vector3 Direction;
+
+    public List<GameObject> Powerups = new List<GameObject>();
+
+    // Use this for initialization
+    void Start () {
+
+        Direction.x = Random.Range(-1f, 1f);
+        Direction.y = Random.Range(-1f, 1f);
+
+        Direction.x *= speed;
+        Direction.y *= speed;
+
+        player = GameObject.Find("Player").GetComponent<Player_Controller>();
 		freezing = false;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		//player = GameObject.Find("Player").GetComponent<Player_Controller>();
-		if (player == null) {
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.gameObject.tag == "Bullet")
+        {
+            float spawn_chance = Random.Range(-1f, 1f);
+            if (spawn_chance > 0)
+            {
+                spawn_chance = Random.Range(-1f, 1f);
+
+                int Powerup_spawn;
+                if (spawn_chance < 0)
+                {
+                    Powerup_spawn = 0;
+                }
+                else
+                {
+                    Powerup_spawn = 1;
+                }
+
+                Instantiate(Powerups[Powerup_spawn].gameObject, transform.position, Quaternion.identity);
+
+            }
+
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        transform.Translate(Direction * Time.deltaTime);
+
+        //player = GameObject.Find("Player").GetComponent<Player_Controller>();
+        if (player == null) {
 			print ("NO PLAYER ARGH!");
 			return;
 		}
