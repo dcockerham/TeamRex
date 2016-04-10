cfe19438be5e5b5cf3fd5e5fbb2c0f24f07eb897
AlvieHuang <Alvinh7@uci.edu>
2016-04-09 17:48:45 -0700

    Stuff

--- Assets/Scripts/Boundary/Boundary_right.cs
+++ Assets/Scripts/Boundary/Boundary_right.cs
@@ -4,11 +4,16 @@ using System.Collections;
 public class Boundary_right : MonoBehaviour {
     void Start()
     {
-        transform.position = new Vector3(Camera.main.orthographicSize * Camera.main.aspect, 0f,0f);
+        transform.position = new Vector3(Camera.main.orthographicSize * Camera.main.aspect*1.1f, 0f,0f);
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         other.gameObject.transform.position -= new Vector3((float)(2 * other.gameObject.transform.position.x - .5), 0f, 0f);
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        other.gameObject.transform.position -= new Vector3((float)(2 * other.gameObject.transform.position.y - 1.5), 0f, 0f);
     }
 }
